b919b896b8d80d8ed79e5b74f1bb211550cd4cd2
Basit <devxbasit@gmail.com>
2024-06-03 16:47:10 +0530

    header & footer done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { User } from '../models/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  authService = inject(AuthService);
+  isLoggedIn = false;
+  private userSubject: Subscription;
 
+  ngOnInit(): void {
+    this.userSubject = this.authService.userSubject.subscribe((user: User) => {
+      this.isLoggedIn = user ? true : false;
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.userSubject.unsubscribe();
+  }
 }
